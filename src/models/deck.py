64a8ae4df0db2b28498a167184976f08a1b67e23
Grum5 <al22760591@ite.edu.mx>
2025-05-27 14:14:28 -0700

    add: Propiedad is_empty agregada, y metodo draw

--- src/models/deck.py
+++ src/models/deck.py
@@ -14,6 +14,18 @@ class Deck:
         self.cards = self._create_deck()
         self.shuffle()
 
+    @property
+    def is_empty(self) -> bool:
+        ''' Property para verificar que el mazo no este vacio '''
+
+        return len(self.cards) == 0
+
+    def reset(self) -> None:
+        ''' Metodo para reiniciar el mazo '''
+
+        self._create_deck()
+        self.shuffle()
+
     def _create_deck(self):
         '''
             Metodo privado
@@ -29,9 +41,7 @@ class Deck:
         return [Card(value, suit) for suit in suits for value in values]
 
     def shuffle(self) -> None:
-        '''
-            Metodo para revolver las cartas del deck
-        '''
+        ''' Metodo para revolver las cartas del deck '''
 
         for i in range(len(self.cards)):
             # Se obtiene un numero pseudoaletorio
@@ -45,3 +55,9 @@ class Deck:
             # Se revuelven las cartas
             self.cards[i], self.cards[j] = self.cards[j], self.cards[i]
 
+    def draw(self) -> Card:
+        ''' Remueve y retorna la carta superior del mazo '''
+
+        if self.cards:
+            return self.cards.pop()
+        raise IndexError("El mazo esta vacio.")

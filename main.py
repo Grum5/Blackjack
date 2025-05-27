fe0b1ced9dfa910c53f72a8529565584897f9c59
Grum5 <al22760591@ite.edu.mx>
2025-05-27 12:49:00 -0700

    improve: Se modularizo la clase Game

--- main.py
+++ main.py
@@ -1,39 +1,4 @@
-import pygame
-
-from src.views.screen import Screen
-
-
-class Game:
-    '''
-        Clase principal del juego
-    '''
-
-    def __init__(self) -> None:
-        ''' Constructor '''
-
-        self.screen = Screen(500, 800, "test")
-        self.running = True
-
-    def run(self) -> None:
-        '''
-            Loop principal del juego
-        '''
-
-        while self.running:
-            self.handle_events()
-            self.screen.fill()
-            self.screen.update()
-
-    def handle_events(self) -> None:
-        '''
-            Manejo de eventos del juego
-            ¡¡ Se debe implementar en controllers !!
-        '''
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-
+from src.controllers.game import Game
 
 if __name__ == '__main__':
 

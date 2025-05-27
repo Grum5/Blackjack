19b526269009891d09f198dc5dd5773c4572cb7d
Grum5 <al22760591@ite.edu.mx>
2025-05-27 14:15:10 -0700

    update: Se actualizo el modelo para recibir numeros en un rango definido de decimales para evitar un StackOverFlow

--- src/models/prng/henon.py
+++ src/models/prng/henon.py
@@ -5,7 +5,12 @@ class Henon:
         mediante el mapa de Henon
     '''
 
-    def __init__(self, a: float, b: float, x0: float, y0: float) -> None:
+    def __init__(
+            self,
+            a: float, b: float,
+            x0: float, y0: float,
+            iterations: int
+    ) -> None:
         ''' Constructor '''
 
         # Parametros de entrada para el modelo
@@ -17,19 +22,22 @@ class Henon:
         # Listas donde se almacenaran los datos del modelo
         self.x_array, self.y_array = [], []
 
+        for _ in range(iterations):
+            _, _ = self.get_pseudonumber()
+
     def get_pseudonumber(self) -> tuple[float, float]:
         '''
             Generación de un numero pseudoaleatorio
             - Retorna una tupla de numeros pseudoaleatorio de tipo flotante
         '''
 
+        # Saturación de valores para evitar overflow
+        MAX_VALUE = 1e6
+        self._x0 = max(min(self._x0, MAX_VALUE), -MAX_VALUE)
+        self._y0 = max(min(self._y0, MAX_VALUE), -MAX_VALUE)
+
         x1 = round(1 - self._a * pow(self._x0, 2) + self._y0, 9)
         y1 = round(self._b * self._x0, 9)
 
         self._x0, self._y0 = x1, y1
-
-        # Almacenamos los datos en la lista
-        self.x_array.append(x1)
-        self.y_array.append(y1)
-
         return x1, y1

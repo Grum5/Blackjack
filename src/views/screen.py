1448428df36adbcb0e4c5327c36c7804ef59ec5a
Grum5 <al22760591@ite.edu.mx>
2025-05-26 22:41:25 -0700

    add: Nueva clase Screen, para el manejo de la ventana

--- src/views/screen.py
+++ src/views/screen.py
@@ -3,5 +3,36 @@ import pygame
 
 class Screen:
 
-    def __init__(self):
-        pass
+    def __init__(self, width: str, height: str, title: str) -> None:
+        ''' Constructor '''
+
+        # Atributos iniciales
+        self.width = width
+        self.height = height
+        self.title = title
+
+        self.surface = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption(self.title)
+
+    def update(self) -> None:
+        ''' Metodo para actualizar la pantalla '''
+
+        pygame.display.flip()
+
+    def fill(self,  color: tuple) -> None:
+        ''' Metodo para rellenar la superficie con un color '''
+
+        self.surface.fill(color)
+
+    def get_surface(self) -> pygame.Surface:
+        '''
+            Metodo que retorna la superficie principal
+            para poder dibujar sobre ella
+        '''
+
+        return self.surface
+
+    def get_size(self) -> tuple:
+        ''' Metodo para retornar el tamaño de la pantalla '''
+
+        return (self.width, self.height)

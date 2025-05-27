8c1231a3f2baa69d1e06690af29b906c3ef6bf71
Grum5 <al22760591@ite.edu.mx>
2025-05-26 22:42:57 -0700

    update: se actualizo y reorganizo la logica de main.py para seguir el modelo MVC

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import pygame
+
+from src.views.screen import Screen
+
 
 class Game:
     '''
@@ -7,6 +11,7 @@ class Game:
     def __init__(self) -> None:
         ''' Constructor '''
 
+        self.screen = Screen(500, 500, "test")
         self.running = True
 
     def run(self) -> None:
@@ -15,7 +20,19 @@ class Game:
         '''
 
         while self.running:
-            pass
+            self.handle_events()
+            self.screen.fill((0, 0, 0))  # Color negro test
+            self.screen.update()
+
+    def handle_events(self) -> None:
+        '''
+            Manejo de eventos del juego
+            ¡¡ Se debe implementar en controllers !!
+        '''
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
 
 
 if __name__ == '__main__':

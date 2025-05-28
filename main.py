afd9998983b207f219c4911cf9057d39143b15f8
JoskaSF <al22760905@ite.edu.mx>
2025-05-27 18:48:30 -0700

    fix: initialize pygame before running game

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from src.controllers.game import Game
+import pygame
 
 if __name__ == '__main__':
-
+    # Inicializar pygame y sus módulos
+    pygame.init()
+    
     # Instanciar el juego y correrlo
     game = Game()
     game.run()

6bdc64a2954ac32d645effa901cd75f70664ad36
Grum5 <al22760591@ite.edu.mx>
2025-05-27 18:52:09 -0700

    add: Se agrego funcion para renderizar las instrucciones de los controles en texto

--- src/views/renderer.py
+++ src/views/renderer.py
@@ -5,16 +5,21 @@ from src.models.dealer import Dealer
 from src.views.screen import Screen
 from src.views.card_renderer import CardRenderer
 
+from typing import Tuple
+
 
 class Renderer:
 
     def __init__(self, screen: Screen) -> None:
         ''' Constructor '''
 
+        pygame.font.init()
+
         self.screen = screen
         self.card_render = []
+        self.font = pygame.font.SysFont("Arial", 20)
         self.spacing = 100
-        self.y_player = 600
+        self.y_player = 625
         self.y_dealer = -100
 
     def draw_hand(self, name: str, hand: object) -> None:
@@ -53,6 +58,34 @@ class Renderer:
                 face_up=False
             )
 
+    def draw_instructions(self) -> None:
+        ''' Dibuja las instrucciones sobre los controles '''
+
+        instructions = [
+            "Teclas:",
+            "H - Pedir carta,",
+            "S - Plantarse,",
+            "Q - Salir",
+        ]
+
+        x = 50
+        y = 580
+
+        padding = 10
+
+        for i, text in enumerate(instructions):
+            text_width = self.font.size(text)[0]
+            self.draw_text(text, (x, y))
+            x += text_width + padding
+
+    def draw_text(self, text: str, pos: Tuple[int, int]) -> None:
+        ''' Metodo para renderizar texto en pantalla '''
+
+        text_surface = self.font.render(text, True, (255, 255, 255))
+        surface = self.screen.get_surface()
+
+        surface.blit(text_surface, pos)
+
     def render_all(self, player: Player, dealer: Dealer, deck: Deck) -> None:
         ''' Metodo para renderizar todos los elementos en pantalla '''
 
@@ -60,3 +93,6 @@ class Renderer:
         self.draw_deck(deck)
         self.draw_hand(player.name, player.hand)
         self.draw_hand(dealer.name, dealer.hand)
+
+        # Se dibujan los controles
+        self.draw_instructions()
